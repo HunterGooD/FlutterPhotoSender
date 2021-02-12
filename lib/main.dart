f8d13162e70ade44abb942e301edf5903d94c3c3
Gudov Vladislav <vlad.gudov@inbox.ru>
2021-02-12 13:36:16 +0400

    del junk

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:flutter/rendering.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:flutter_camera/internal/application.dart';
 
 void main() async {
